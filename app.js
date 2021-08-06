731c0d477cbebc652e43ea9a64758b65a8f516fb
Sh4dowMonkey <cedric.elie.harb@gmail.com>
2021-08-06 20:17:14 +0300

    feat: winning logic

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
-const container = document.getElementById("grid-plate");
+const templateBurger = ["bottom-bun", "beef-patty", "top-bun"];
+let currentBurger = [];
 
-const onPress = (id) => {
+function removeItem(id) {
+  currentBurger = currentBurger.filter(function (value, index, array) {
+    return value != id;
+  });
+}
+
+let i = 1;
+
+const onPressIngredients = (id) => {
   const element = document.getElementById(id);
-  container.appendChild(element);
+  const container = element.parentNode.id;
+  if (container !== "grid-ingredients") {
+    const newContainer = document.getElementById("grid-ingredients");
+    newContainer.appendChild(element);
+    element.style.top = "0px";
+    i--;
+    removeItem(id);
+  } else {
+    const newContainer = document.getElementById("grid-plate");
+    newContainer.appendChild(element);
+    element.style.top = 40 * i + "px";
+    i++;
+    currentBurger.push(id);
+  }
+};
+
+const onPressCheck = () => {
+  if (JSON.stringify(currentBurger) === JSON.stringify(templateBurger)) {
+    alert("Congratulations!");
+  } else {
+    alert("You're not done yet!");
+  }
 };
